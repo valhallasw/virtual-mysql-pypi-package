6bb3596d5249a66114ee6724c2f86c508e538cce
Merlijn van Deen <valhallasw+lisilwen@gmail.com>
2018-10-02 21:05:15 +0200

    Require mysqlclient for python3; add warning notice
    Closes #1

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Copyright (c) <year> <copyright holders>
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -25,6 +25,11 @@ try:
 except ImportError:
     from distutils.core import setup
 
+import sys
+package = "MySQL-python" if sys.version_info.major < 3 else "mysqlclient"
+
+print("WARNING: `mysql` is a virtual package. Please use `%s` as a dependency directly.\n")
+
 setup(
     name='mysql',
     version='0.0.1',
@@ -32,7 +37,7 @@ setup(
     long_description=open('README.rst').read(),
     author='Merlijn van Deen',
     author_email='valhallasw@arctus.nl',
-    install_requires=["MySQL-python"],
-    license=["GPL"],
+    install_requires=[package],
+    license=["MIT"],
     url="https://github.com/valhallasw/virtual-mysql-pypi-package",
 )
